43c008a3b3777f66e9faa2c2647273f448366ba9
Nathaniel Doromal <nathanielrd98@gmail.com>
2021-04-26 01:25:12 -0700

    added test script to Unity Project

---
+++ Assets/Scripts/DELETE_testScript_Niel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DELETE_testScript_Niel : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
