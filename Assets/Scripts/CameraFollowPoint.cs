c9e5ae9598e728120f281ffc35fe683ca974167b
Nathaniel Doromal <nathanielrd98@gmail.com>
2021-04-26 00:58:59 -0700

    adding all parts of unity project

---
+++ Assets/Scripts/CameraFollowPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowPoint : MonoBehaviour
+{
+    public GameObject Player;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.position = Player.transform.position;
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public bool Grounded = false;
+    
+    void OnTriggerStay(Collider other)
+    {
+        if(other.tag != "Player")
+        {
+            Grounded = true;
+        }        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag != "Player")
+        {
+            Grounded = false;
+        }    
+    }
+}
