dc3f4c1d6e7b4d145f7d1ddf37e7f06d53ee48cf
GuruDaddy <81791351+DaGuruDaddy@users.noreply.github.com>
2021-04-27 21:19:50 -0700

    Hi
    This is for the new jazz

---
+++ Assets/Scripts/GrabBox.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabBox : MonoBehaviour
+{
+    public bool SensingEquippable = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Grabbable")
+        {
+            SensingEquippable = true;
+        }    
+        if(other.tag == "Throwable")
+        {
+
+        }
+    }
+    
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Grabbable")
+        {
+            SensingEquippable = false;
+        }
+        if(other.tag == "Throwable")
+        {
+
+        }
+    }
+}
