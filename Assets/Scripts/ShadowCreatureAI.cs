a39684a009712e1541d840cd6fb59e761a594ac2
GuruDaddy <81791351+DaGuruDaddy@users.noreply.github.com>
2021-05-10 21:43:21 -0700

    Badda Bing Baby

---
+++ Assets/Scripts/ShadowCreatureAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class ShadowCreatureAI : MonoBehaviour
+{
+    private NavMeshAgent ShadowCreature;
+    public Animator Animator;
+    public GameObject Player;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ShadowCreature = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (ShadowCreature.isOnOffMeshLink)
+            Animator.Play("SC_Jump");
+        ShadowCreature.SetDestination(Player.transform.position);
+        if (Input.GetKeyDown("p"))
+        {
+            Animator.Play("SC_Jump");
+        }
+
+    }
+}
