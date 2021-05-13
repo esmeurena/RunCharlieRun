6d24cd178c05002d4d648d79b02aef86f23c3396
GuruDaddy <81791351+DaGuruDaddy@users.noreply.github.com>
2021-05-12 18:16:15 -0700

    Third Level

---
+++ Assets/BeginningFallColliderScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginningFallColliderScript : MonoBehaviour
+{
+    public CameraFollowing Camera;
+    public CharlieController1 CC1S;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+            CC1S.DisabledMovement = true;
+    }
+}
