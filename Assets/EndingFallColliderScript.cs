48caa4710aaac3ce83244359fb0a4c180b987d19
GuruDaddy <81791351+DaGuruDaddy@users.noreply.github.com>
2021-05-12 18:37:40 -0700

    Drew's Change

---
+++ Assets/EndingFallColliderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndingFallColliderScript : MonoBehaviour
+{
+    public CharlieController1 CC1S;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            CC1S.DisabledMovement = false;
+        }    
+    }
+}
